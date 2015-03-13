88b74e253407c3282d5c8df290ec6eb52a01ed90
Sam <rufus.khan@gmail.com>
2015-03-13 18:46:21 -0400

    Guitarduino Core
    Core software for running the Guitarduino board. Reads a sample from the
    ADC, transforms it to the DAC bit depth and outputs it. Repeats at a
    configurable sample rate.

---
+++ Guitarduino.ino
@@ -0,0 +1,47 @@
+/*  GUITARDUINO Digital Guitar Effects Processor
+    Copyright (C) 2011-2015  Samuel Brown
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+const int signalPin = A0; //establish pin 0 as the analog input
+
+int sampleIn = 0; //sets up a variable to store the sample
+byte sampleOut = 0; //sets up a variable to convert the sample to 6 bits
+
+
+
+void DAConvert(byte sample);//sends a 6 bit sample to the DAC
+
+
+
+void DAConvert(byte sample)
+{
+  PORTB = sample;
+}
+
+void setup()
+{
+  //Serial.begin(9600);
+  DDRB = B111111;
+}
+
+
+void loop()
+{
+  sampleIn = analogRead(signalPin); //sample the input
+  sampleOut = map(sampleIn, 0, 1023, 0, 63); //convert the sample to 6 bits
+  DAConvert(sampleOut); //send the sample to the DAC
+  delayMicroseconds(80); //it takes 100 microseconds to sample, adjust for best sound
+}
+
