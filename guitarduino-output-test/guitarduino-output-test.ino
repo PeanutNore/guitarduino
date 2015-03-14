d90c8d6aba2723089d60166062e5c4ce95ed5f09
Sam <rufus.khan@gmail.com>
2015-03-13 21:11:50 -0400

    Added license information

--- guitarduino-output-test/guitarduino-output-test.ino
+++ guitarduino-output-test/guitarduino-output-test.ino
@@ -1,3 +1,19 @@
+/*  GUITARDUINO Digital Guitar Effects Processor
+    Copyright (C) 2011-2015  Samuel Brown
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
 const int signalPin = A0; //establish pin 0 as the analog input
 
 byte sampleOut = 0; //sets up a variable to convert the sample to 6 bits
@@ -15,7 +31,7 @@ void DAConvert(byte sample)
 
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   DDRB = B111111;
 }
 
@@ -24,10 +40,6 @@ void loop()
 {
   DAConvert(sampleOut); //send the sample to the DAC
   sampleOut++;
-  //Serial.print(sampleIn);
-  //Serial.print(' ');
-  //Serial.println(int(sampleOut));
-  //delay(100);
   delayMicroseconds(100); //it takes 100 microseconds to sample
 }
 
