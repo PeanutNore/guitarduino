ee9caf99803902ee1edc74235566203d0265b786
Sam <rufus.khan@gmail.com>
2015-03-13 21:06:58 -0400

    Update guitarduino-comp.ino

--- guitarduino-comp/guitarduino-comp.ino
+++ guitarduino-comp/guitarduino-comp.ino
@@ -3,11 +3,7 @@ const int signalPin = A0; //establish pin 0 as the analog input
 int sampleIn = 0; //sets up a variable to store the sample
 byte sampleOut = 0; //sets up a variable to convert the sample to 6 bits
 
-
-
 void DAConvert(byte sample);//sends a 6 bit sample to the DAC
-
-
 
 void DAConvert(byte sample)
 {
@@ -16,7 +12,7 @@ void DAConvert(byte sample)
 
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   DDRB = B111111;
 }
 
@@ -24,14 +20,11 @@ void setup()
 void loop()
 {
   sampleIn = analogRead(signalPin); //sample the input
-  sampleIn = map(sampleIn, 0, 1023, -512, 511);
-  sampleIn = ((2*sampleIn)/512+abs(sampleIn));
-  sampleOut = map(sampleIn, -512, 512, 0, 63); //convert the sample to 6 bits
+  sampleIn = map(sampleIn, 0, 1023, -512, 511); //center the sample around 0
+  sampleIn = ((2*sampleIn)/512+abs(sampleIn)); //simple compression algorithm
+  sampleIn = contstrain(sampleIn, -512, 511); // constrain sample within 10 bits to prevent rolling over on next map
+  sampleOut = map(sampleIn, -512, 511, 0, 63); //convert the sample to 6 bits
   DAConvert(sampleOut); //send the sample to the DAC
-  //Serial.print(sampleIn);
-  //Serial.print(' ');
-  //Serial.println(int(sampleOut));
-  //delay(100);
-  delayMicroseconds(100); //it takes 100 microseconds to sample
+  delayMicroseconds(50); //it takes 100 microseconds to sample
 }
 
