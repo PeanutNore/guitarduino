63c2e61748ae31e9ba74951424bdc1bbc85520fd
Sam <rufus.khan@gmail.com>
2015-03-19 10:48:24 -0400

    Redesign for better operation
    Designed to use 2 pairs of switches to control 2 different settings (four modes for each).
    Use loops in setup() to run specific code for a mode setting.
    Running mode is changed by resetting the device.
    loop() contains default I/O program to run if nothing is coded for mode settings in use.

--- guitarduino-mode-switching/guitarduino-mode-switching.ino
+++ guitarduino-mode-switching/guitarduino-mode-switching.ino
@@ -16,10 +16,12 @@
 
 const int signalPin = A0; //establish pin 0 as the analog input
 
-unsigned int sampleIn = 0; //sets up a variable to store the sample
-byte sampleOut = 0; //sets up a variable to convert the sample to 6 bits
-byte modeSwtiches = 0; //variable to store mode switch setting
-
+unsigned int sampleIn = 0; //Stores the sample from the ADC
+byte sampleOut = 0; //Stores the sample to send to the DAC
+byte modeInput = 0; //Storse mode switch settings
+byte modeA = 0; //Stores the highest 2 mode switches, "Mode A" on the device
+byte modeB = 0; //Stores the lowest 2 mode switches, "Mode B" on the device
+const boolean runLoop = true; //If a loop is running, it should run until the device is reset. Might not be needed.
 
 void DAConvert(byte sample);//sends a 6 bit sample to the DAC
 
@@ -34,24 +36,27 @@ void setup()
 {
   //Serial.begin(9600);
   DDRB = B111111; //sets Port B to output
-  DDRD = B00000010; //sets Port D to input
+  DDRD = B00000010; //sets Port D to input on high bits - bits 0 and 1 are serial
+  modeInput = PIND; //reads the mode switches on Port D
+  modeInput = modeInput >> 2; //shift over to lowest bits
+  modeA = modeInput & B00001100;
+  modeA = modeA >> 2;
+  modeB = modeInput & B00000011;
+  /* create loops here to do something different based on the modes
+  Example:
+  while (modeA & 1 == B11)//Runs forever if Mode A was set to "1"
+  {
+      //do stuff
+  }
+  End Example */
 }
 
 
 void loop()
 {
-  modeSwitches = PIND; //reads the mode switches on Port D
-  modeSwitches = modeSwitches >> 2;
-  swtich (modeSwitches)
-    {
-    //case B0000:
-    //
-    //break;
-    default:
-    sampleIn = analogRead(signalPin); //sample the input
-    sampleOut = sampleIn >> 4; //toss the 4 least significant bits
-    DAConvert(sampleOut); //send the sample to the DAC
-    }
+  sampleIn = analogRead(signalPin); //sample the input
+  sampleOut = sampleIn >> 4; //toss the 4 least significant bits
+  DAConvert(sampleOut); //send the sample to the DAC
   //delayMicroseconds(80); //it takes about 115 microseconds to run the above code, add delay here to decrease sample rate
 }
 
