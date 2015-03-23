d7f4aa6c08db2e07878862a67e2f8e00bd1e9d17
Sam <rufus.khan@gmail.com>
2015-03-23 14:01:22 -0400

    Added modes
    Added modes 1-3 for bitcrushing functions

--- guitarduino-mode-switching/guitarduino-mode-switching.ino
+++ guitarduino-mode-switching/guitarduino-mode-switching.ino
@@ -18,9 +18,7 @@ const int signalPin = A0; //establish pin 0 as the analog input
 
 unsigned int sampleIn = 0; //Stores the sample from the ADC
 byte sampleOut = 0; //Stores the sample to send to the DAC
-byte modeInput = 0; //Storse mode switch settings
-byte modeA = 0; //Stores the highest 2 mode switches, "Mode A" on the device
-byte modeB = 0; //Stores the lowest 2 mode switches, "Mode B" on the device
+byte mode = 0; //Storse mode switch settings
 const boolean runLoop = true; //If a loop is running, it should run until the device is reset. Might not be needed.
 
 void DAConvert(byte sample);//sends a 6 bit sample to the DAC
@@ -37,18 +35,33 @@ void setup()
   //Serial.begin(9600);
   DDRB = B111111; //sets Port B to output
   DDRD = B00000010; //sets Port D to input on high bits - bits 0 and 1 are serial
-  modeInput = PIND; //reads the mode switches on Port D
-  modeInput = modeInput >> 2; //shift over to lowest bits
-  modeA = modeInput & B00001100;
-  modeA = modeA >> 2;
-  modeB = modeInput & B00000011;
-  /* create loops here to do something different based on the modes
-  Example:
-  while (modeA & 1 == B11)//Runs forever if Mode A was set to "1"
+  mode = PIND >> 2; //reads the mode switches on Port D
+  // create loops here to do something different based on the modes
+  //Mode 1: Reduce bit depth only
+  while (mode = 1)
   {
-      //do stuff
+      sampleIn = analogRead(signalPin); //sample the input
+      sampleOut = sampleIn >> 6; //toss the 6 least significant bits
+      sampleOut = sampleIn <<2; //shift to most significant bits for output
+      DAConvert(sampleOut); //send the sample to the DAC
   }
-  End Example */
+  //Mode 2: Reduce sample rate only
+  while (mode= = 2)
+  {
+      sampleIn = analogRead(signalPin); //sample the input
+      sampleOut = sampleIn >> 4; //toss the 4 least significant bits
+      DAConvert(sampleOut); //send the sample to the DAC
+      delayMicroseconds(224); //it takes about 115 microseconds to run the above code, add delay here to decrease sample rate
+  }
+  //Mode 3: Reduce bit depth and sample rate
+  while (mode = 3)
+  {
+      sampleIn = analogRead(signalPin); //sample the input
+      sampleOut = sampleIn >> 6; //toss the 6 least significant bits
+      sampleOut = sampleIn <<2; //shift to most significant bits for output
+      DAConvert(sampleOut); //send the sample to the DAC
+      delayMicroseconds(224); //it takes about 115 microseconds to run the above code, add delay here to decrease sample rate
+  }
 }
 
 
