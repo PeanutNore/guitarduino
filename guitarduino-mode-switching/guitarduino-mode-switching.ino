938d938c2d3f92ee5279db490c075ab3182809dd
Sam <rufus.khan@gmail.com>
2015-03-31 11:06:31 -0400

    Fixed variable placement
    Wasn't working because variables were being destroyed each time the loop ran

--- guitarduino-mode-switching/guitarduino-mode-switching.ino
+++ guitarduino-mode-switching/guitarduino-mode-switching.ino
@@ -15,8 +15,15 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 const int signalPin = A0; //establish pin 0 as the analog input
-
+const float b[19] = {-0.002693840, -0.002519748, 0.005014695, 0.015641050, 0.000000000, 
+                    -0.046914239, -0.048021820, 0.083481298, 0.294332820, 0.400000000, 
+                    0.294332820, 0.083481298, -0.048021820, -0.046914239, 0.000000000,
+                    0.015641050, 0.005014695, -0.002519748, -0.002693840}; 
+                    // Incants the arcane numbers (Coefficients for Goertzel Algorithm)
+float sampleFloat = 0;
 unsigned int sampleIn = 0; //Stores the sample from the ADC
+unsigned int sampleSum = 0; //Stores the sum used in filter algorithm
+unsigned int sampleBuffer[19]; //buffers samples used in filter algorithm
 int sampleAdj = 0; // For "Adjusting" the sample when negative values are needed
 byte sampleOut = 0; //Stores the sample to send to the DAC
 byte modeSetting = 0; //Storse mode switch settings
@@ -33,26 +40,17 @@ void DAConvert(byte sample)
 
 void LowPassFilter()
 {
-  const float b[19] = {-0.002693840, -0.002519748, 0.005014695, 0.015641050, 0.000000000, 
-                    -0.046914239, -0.048021820, 0.083481298, 0.294332820, 0.400000000, 
-                    0.294332820, 0.083481298, -0.048021820, -0.046914239, 0.000000000,
-                    0.015641050, 0.005014695, -0.002519748, -0.002693840}; 
-                    // Incants the arcane numbers (Coefficients for Goertzel Algorithm)
-  unsigned int sampleInF = 0;
-  unsigned int sampleSum = 0;
-  unsigned int sampleBuffer[19];
-  
-  sampleInF = analogRead(signalPin); //sample the input
+  sampleIn = analogRead(signalPin); //sample the input
   for (int i = 1; i < 18; i++) //shift all the samples in the buffer one position lower
   {
     sampleBuffer[i-1] = sampleBuffer[i];
   }
-  sampleBuffer[18] = sampleInF; //add current sample to buffer as most recent
+  sampleBuffer[18] = sampleIn; //add current sample to buffer as most recent
   for (int i = 0; i < 19; i++) //Takess the sum of all samples in the buffer * their coefficients
   {
-    sampleSum += b[i] * sampleBuffer[18-i];
+    sampleFloat += b[i] * sampleBuffer[18-i];
   }
-  sampleSum = constrain(sampleSum, 0, 1023); //Probably not necessary, but try adding this if the output sounds weird
+  sampleSum = constrain(sampleFloat, 0, 1023); //turn the sample into an unsigned int in the appropriate range
   sampleOut = sampleSum >> 4;
   //sampleOut = 63 - sampleOut; //Makes it a high pass filter instead of low pass
   DAConvert(sampleOut); //send the sample to the DAC
@@ -61,30 +59,22 @@ void LowPassFilter()
 
 void HighPassFilter()
 {
-  const float b[19] = {-0.002693840, -0.002519748, 0.005014695, 0.015641050, 0.000000000, 
-                    -0.046914239, -0.048021820, 0.083481298, 0.294332820, 0.400000000, 
-                    0.294332820, 0.083481298, -0.048021820, -0.046914239, 0.000000000,
-                    0.015641050, 0.005014695, -0.002519748, -0.002693840}; 
-                    // Incants the arcane numbers (Coefficients for Goertzel Algorithm)
-  unsigned int sampleInF = 0;
-  unsigned int sampleSum = 0;
-  unsigned int sampleBuffer[19];
-  
-  sampleInF = analogRead(signalPin); //sample the input
+  sampleIn = analogRead(signalPin); //sample the input
   for (int i = 1; i < 18; i++) //shift all the samples in the buffer one position lower
   {
     sampleBuffer[i-1] = sampleBuffer[i];
   }
-  sampleBuffer[18] = sampleInF; //add current sample to buffer as most recent
+  sampleBuffer[18] = sampleIn; //add current sample to buffer as most recent
   for (int i = 0; i < 19; i++) //Takess the sum of all samples in the buffer * their coefficients
   {
-    sampleSum += b[i] * sampleBuffer[18-i];
+    sampleFloat += b[i] * sampleBuffer[18-i];
   }
-  sampleSum = constrain(sampleSum, 0, 1023); //Probably not necessary, but try adding this if the output sounds weird
+  sampleSum = constrain(sampleFloat, 0, 1023); //Probably not necessary, but try adding this if the output sounds weird
   sampleOut = sampleSum >> 4;
   sampleOut = 63 - sampleOut; //Makes it a high pass filter instead of low pass
   DAConvert(sampleOut); //send the sample to the DAC
   sampleSum = 0; //reset the sum to zero
+  sampleFloat = 0;
 }
 
 
